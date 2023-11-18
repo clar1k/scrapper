b32fa518c6286de0f6ea17e093d7ccbc1466e0f6
Serhii Khara <112571762+clar1k@users.noreply.github.com>
2023-11-18 16:16:05 +0200

    feat: update gitignore, add new image_url

--- main.py
+++ main.py
@@ -9,63 +9,70 @@ import uvicorn
 from database import db
 
 
-app = FastAPI(debug=True, title='Hackathon API')
+app = FastAPI(debug=True, title="Hackathon API")
+
 
 class HackathonPost(BaseModel):
-  title: str
-  description: str
-  date: str
-  image_url: str
-  link: str
+    title: str
+    description: str
+    date: str
+    image_url: str
+    link: str
 
 
 def parse_dou() -> list[HackathonPost] or None:
-  URL = 'https://dou.ua/calendar/tags/%D1%85%D0%B0%D0%BA%D0%B0%D1%82%D0%BE%D0%BD/'
+    URL = "https://dou.ua/calendar/tags/%D1%85%D0%B0%D0%BA%D0%B0%D1%82%D0%BE%D0%BD/"
 
-  fake_user_agent = {
-      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36',
-  }
+    fake_user_agent = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36",
+    }
 
-  response = requests.get(URL, headers=fake_user_agent)
-  CONTENT = response.content
+    response = requests.get(URL, headers=fake_user_agent)
+    CONTENT = response.content
 
-  soup = bs4.BeautifulSoup(CONTENT, 'html.parser', from_encoding='utf-8')
+    soup = bs4.BeautifulSoup(CONTENT, "html.parser", from_encoding="utf-8")
 
-  post_cards = soup.find_all('article', attrs={"class": "b-postcard"})
+    post_cards = soup.find_all("article", attrs={"class": "b-postcard"})
 
-  hackathon_posts = []
-  for post in post_cards:
-    text = post.find('a')
-    url = text.attrs['href']
-    
-    header_text: str = text.text
-    header_text = header_text.replace('\t', '')
-    cleaned_text = re.sub(r'\s+', ' ', header_text.strip())
-    
-    description = post.find('p', attrs={"class": "b-typo"})
-    description: str = description.text
-    description = description.replace('\t', '')
-    description = re.sub(r'\s+', ' ', description.strip())
-    
-    image_url = post.find('img')
-    image_url = image_url.attrs['src']
-    
-    date = post.find('span', attrs={"class": "date"})
-    date = date.text
-    
-    
-    hackathon_post = HackathonPost(title=cleaned_text, description=description, date=date, image_url=image_url, link=url)
-    hackathon_posts.append(hackathon_post)
-  return hackathon_posts
+    hackathon_posts = []
+    for post in post_cards:
+        text = post.find("a")
+        url = text.attrs["href"]
+
+        header_text: str = text.text
+        header_text = header_text.replace("\t", "")
+        cleaned_text = re.sub(r"\s+", " ", header_text.strip())
+
+        description = post.find("p", attrs={"class": "b-typo"})
+        description: str = description.text
+        description = description.replace("\t", "")
+        description = re.sub(r"\s+", " ", description.strip())
+
+        image_url = post.find("img")
+        image_url = image_url.attrs["srcset"]
+        image_url = image_url[0 : image_url.find(".png") + 4]
+
+        date = post.find("span", attrs={"class": "date"})
+        date = date.text
+
+        hackathon_post = HackathonPost(
+            title=cleaned_text,
+            description=description,
+            date=date,
+            image_url=image_url,
+            link=url,
+        )
+        hackathon_posts.append(hackathon_post)
+    return hackathon_posts
 
 
-@app.get('/hackathons', response_model=list[HackathonPost])
+@app.get("/hackathons", response_model=list[HackathonPost])
 def current_hackathons():
-  hackathons = parse_dou()
-  for hackathon in hackathons:
-    ic(hackathon)
-  return hackathons
+    hackathons = parse_dou()
+    for hackathon in hackathons:
+        ic(hackathon)
+    return hackathons
 
 
-if __name__ == '__main__':
-  uvicorn.run('main:app')
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
